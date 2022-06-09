dfc044df1ecd0787c7c33853792086a4264f5007
zerom-H <cupdisin@gmail.com>
2022-06-09 18:32:17 +0900

    Initial commit

---
+++ src/main/java/jpabasic/hellojpa/Member.java
@@ -0,0 +1,14 @@
+package jpabasic.hellojpa;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+}
