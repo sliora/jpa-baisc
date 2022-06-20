0f114946dab667b326df6be12c4e6aa5f3698ad7
zerom-H <cupdisin@gmail.com>
2022-06-20 11:02:19 +0900

    실전예제 실습

--- src/main/java/jpabasic/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabasic/jpabook/jpashop/JpaMain.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class JpaMain {
